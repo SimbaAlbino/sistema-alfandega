7afe8a39b3d661ac1f395bf64d0110e6f3fc0179
Pedro Martins <122813242+SimbaAlbino@users.noreply.github.com>
2024-06-27 20:12:04 -0300

    Ultima att cliente

--- alfandega/src/entidades/Cliente.java
+++ alfandega/src/entidades/Cliente.java
@@ -12,22 +12,18 @@ public class Cliente extends Utilizador implements Usuario<Cliente>, Serializabl
 	private String email;
 	private String senha;
 	private String cpf;
-	
 
 	private String caminhoClientesFile = "";
-	
-	
-	//construtor de cadastro
-	public Cliente(String nomeCliente, String email, String senha, String cpf) {
 
-	public Cliente(String nomeCliente, String email, String cpf) {
+	// construtor de cadastro
+	public Cliente(String nomeCliente, String email, String senha, String cpf) {
 		this.nomeCliente = nomeCliente;
 		this.email = email;
 		this.senha = senha;
 		this.cpf = cpf;
 	}
 
-	//gerando construtor de uso para o Vendedor
+	// gerando construtor de uso para o Vendedor
 	public Cliente(String nomeCliente, String cpf) {
 		this.nomeCliente = nomeCliente;
 		this.cpf = cpf;
@@ -37,47 +33,27 @@ public class Cliente extends Utilizador implements Usuario<Cliente>, Serializabl
 		return nomeCliente;
 	}
 
-	public void setNomeCliente(String nomeCliente) {
-		this.nomeCliente = nomeCliente;
-	}
-
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public String getCpf() {
 		return cpf;
 	}
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
 	public String getCaminhoClientesFile() {
 		return caminhoClientesFile;
 	}
-	
-	
-	
-	
-	
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void listarProdutos() {
 		ArrayList<DadosProduto> listaProdutos = new ArrayList<>();
 		try {
-			listaProdutos = (ArrayList<DadosProduto>) ModelagemFile.desserializar(getCaminhoClientesFile()); // CASTING
-																												// DO
-																												// CURINGA
+			listaProdutos = (ArrayList<DadosProduto>) ModelagemFile.desserializar(getCaminhoClientesFile()); // CASTING DO CURINGA
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-
 		for (DadosProduto dadoProduto : listaProdutos) {
 			// se não estiver no estoque, estará no despache
 			if (dadoProduto.getCliente().equals(this)) {
@@ -88,26 +64,22 @@ public class Cliente extends Utilizador implements Usuario<Cliente>, Serializabl
 
 	@Override
 	public void avisosCanal(DadosProduto produto) {
-		System.out.printf("O produto: %s está %s", produto.getTipoItem(), produto.getStatus().name());
-		//Se o produto tiver no estoque e com a coloção amarela, verificar com o funcionario, mensagem;
-		System.out.printf("O produto: %s está %s", produto.getProduto().getClass(), produto.getStatus().name());
-		// usar enum
+		System.out.printf("O produto: %s está %s", produto.getTipoItem().getNome(), produto.getStatus());
+		// Se o produto tiver no estoque e com a coloção amarela, verificar com o funcionario, mensagem;
 	}
 
 	@Override
-	public void avisosCanal() {
+	protected void cadastrarUser() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	protected void pagamento() {
 		// TODO Auto-generated method stub
-
+		
 	}
-	// ao validar o cpf do cliente, apenas considerar os números, se vier uma letra, faz um while.
-	//usar o equals e hashCode de acordo com a necessidade no futuro. em listar produtos precisamos encontrar por Cliente
-
+	// ao validar o cpf do cliente, apenas considerar os números, se vier uma letra faz um while.
 	// usar o equals e hashCode de acordo com a necessidade no futuro. em listar
 	// produtos precisamos encontrar por Cliente
 }
---
+++ alfandega/src/reserva/StatusProduto.java
@@ -0,0 +1,9 @@
+package reserva;
+
+public enum StatusProduto {
+	INEXISTENTE,
+	ESTOQUE,
+	TRANSPORTE,
+	RETORNO,
+	ILEGAL;
+}
