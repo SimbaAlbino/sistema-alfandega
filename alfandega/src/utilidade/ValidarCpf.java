85841dba813c0ceebafa318e61e16a6f3f825511
Gabriel Lopes <gabriel.lopes.albuquerque@gmail.com>
2024-07-03 11:06:43 -0300

    validacao cpf e cep

--- alfandega/src/utilidade/ValidarCpf.java
+++ alfandega/src/utilidade/ValidarCpf.java
@@ -1,5 +1,14 @@
 package utilidade;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidarCpf {
+
+	// Método para validar CPF
+	public static boolean validarCPF(String cpf) {
+		// Verifica se o CPF não é nulo e se possui exatamente 11 dígitos numéricos
+		return cpf != null && cpf.matches("\\d{11}");
+	}
 
 }
